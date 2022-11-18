4026eea8d6b8c262cf5cd83b0f7c383d684f0388
Avery Lin <60205611+avery710@users.noreply.github.com>
2022-11-18 20:17:30 +0800

    Add host = 0.0.0.0

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 import mysql.connector
+import mysql.connector.pooling
 import math
 
 
@@ -222,4 +223,4 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-app.run(port=3000)
+app.run(port=3000, host="0.0.0.0")
