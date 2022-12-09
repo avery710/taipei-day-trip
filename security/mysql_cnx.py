363349ada5fd213b2d8fafc838bf0dd0b30cb8a8
Avery Lin <60205611+avery710@users.noreply.github.com>
2022-12-09 16:26:09 +0800

    Add Modules

---
+++ security/mysql_cnx.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import mysql.connector.pooling
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def open_cnx_pool():
+    dbconfig = {
+        "host" : "localhost",
+        "user" : os.getenv('db_user'),
+        "password" : os.getenv('db_pw'),
+        "database" : "taipei_trip"
+    }
+
+    cnx_pool = mysql.connector.pooling.MySQLConnectionPool(
+        pool_name = "mysql_pool",
+        pool_size = 5,
+        autocommit = True,
+        **dbconfig
+    )
+
+    return cnx_pool
