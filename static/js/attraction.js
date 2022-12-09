e5d6d6f9767d46827cf7e0a2bb2231ec10643f61
Avery Lin <60205611+avery710@users.noreply.github.com>
2022-12-09 16:27:03 +0800

    Add Member-login Nav-bar Function

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -9,11 +9,46 @@ let address = document.getElementById('address')
 let transport = document.getElementById('transport')
 let expense = document.getElementById('expense-result')
 
-const root_url = "http://52.69.53.123:3000/"
-// const root_url = "http://127.0.0.1:3000/"
+
+const login_prompt = document.getElementById('login-prompt')
+const signup_prompt = document.getElementById('signup-prompt')
+const login_section = document.getElementById('login')
+const signup_section = document.getElementById('signup')
+
+const close_buttons = document.querySelectorAll('.close-button')
+const overlay_section = document.querySelector('.overlay')
+
+const overlay_trigger = document.getElementById('overlay-trigger')
+const logout = document.getElementById('logout')
+
+const login_form = document.getElementById('login-form')
+const signup_form = document.getElementById('signup-form')
+
+
+window.onload = function(){
+    // check whether the user is login
+    fetch('/api/user/auth', {
+        credentials: 'include'
+    })
+    .then(res => res.json())
+    .then(data => {
+        const user_data = data['data']
+
+        if (user_data == null){
+            overlay_trigger.style.display = "inline"
+            logout.style.display = "none"
+        }
+        else {
+            logout.style.display = "inline-block"
+            overlay_trigger.style.display = "none"
+        }
+    }).catch(error => {
+        console.log(`error: ${error}`)
+    })
+}
+
 
 let slideIndex = 1
-
 
 function showSlides(n) {
     let slides = document.querySelectorAll('.slide-img')
@@ -83,7 +118,7 @@ document.querySelectorAll('input[name="booking-time"]').forEach(radio => {
 
 
 // fetch basic info about the attraction
-fetch(`${root_url}api/attraction/${id}`)
+fetch(`/api/attraction/${id}`)
 .then(res => res.json())
 .then(data => {
     let attraction = data['data']
@@ -110,6 +145,108 @@ fetch(`${root_url}api/attraction/${id}`)
 
     slideIndex = 1
     showSlides(slideIndex)
-    console.log("hihi")
 })
 .catch(error => console.log(error))
+
+
+
+
+login_prompt.addEventListener('click', e => {
+    login_section.style.display = 'none'
+    signup_section.style.display = 'flex'
+})
+
+
+signup_prompt.addEventListener('click', e => {
+    signup_section.style.display = 'none'
+    login_section.style.display = 'flex'
+})
+
+
+close_buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        overlay_section.style.display = 'none'
+    })
+})
+
+
+overlay_trigger.addEventListener('click', e => {
+    overlay_section.style.display = 'flex'
+})
+
+
+login_form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const email = document.getElementById('login-email').value
+    const pw = document.getElementById('login-pw').value
+
+    const error_message = document.querySelector('#login .error-message')
+
+    // Build formData object.
+    let formData = new FormData()
+    formData.append('password', pw)
+    formData.append('email', email)
+
+    fetch(`/api/user/auth`, {
+        method: "PUT",
+        body: formData,
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data['error'] == true){
+            error_message.textContent = data['message']
+        }
+        else if (data['ok'] == true) {
+            window.location.reload();
+        }
+    })
+    .catch(error => console.log(error))
+})
+
+
+signup_form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const email = document.getElementById('signup-email').value
+    const pw = document.getElementById('signup-pw').value
+    const name = document.getElementById('signup-name').value
+
+    const error_message = document.querySelector('#signup .error-message')
+
+    // Build formData object.
+    let formData = new FormData()
+    formData.append('name', name)
+    formData.append('password', pw)
+    formData.append('email', email)
+
+    fetch(`/api/user`, {
+        method: "POST",
+        body: formData,
+    })
+    .then(res => res.json())
+    .then(data => {
+        // error -> show error-message
+        if (data['error'] == true){
+            error_message.textContent = data['message']
+        }
+        else if (data['ok'] == true) {
+            error_message.textContent = "註冊成功！"
+        }
+    })
+    .catch(error => console.log(error))
+})
+
+
+logout.addEventListener('click', e => {
+    fetch(`/api/user/auth`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data['ok'] == true){
+            window.location.reload();
+        }
+    })
+    .catch(error => console.log(error))
+})
